d60ce6b78b30081aae5bec573c1783135253c853
Todd Libby <toddlibby@protonmail.com>
2026-02-15 22:53:22 -0700

    Update to Nuxt 4.3.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase'],
